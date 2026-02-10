15e91d0c67909c979350d7fe7136911e2cc2db5f
Nivas L <nivasloganathan@gmail.com>
2026-02-10 19:42:58 +0530

    Add files via upload

---
+++ print output in two lines using n.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Print output in two lines using \n
+Test Case 1
+Output:
+Hello
+world
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\nworld");
+    return 0;
+}
+
