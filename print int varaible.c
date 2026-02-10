900714dd535d85360db1120f7b6bd7045c373658
Nivas L <nivasloganathan@gmail.com>
2026-02-10 21:03:28 +0530

    Add files via upload

---
+++ print int varaible.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+ Topic 3: Data Types in C – Test Cases
+1. Print int variable
+Test Case 1
+Output: 10
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("%d", a);
+    return 0;
+}
