4aaa313e2bbc5b81543a605b46d744852ff86f6e
Nivas L <nivasloganathan@gmail.com>
2026-02-10 19:49:08 +0530

    Add files via upload

---
+++ Declare an interger variable and print it.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Topic 2: Variables in C – Test Cases
+1. Declare an integer variable and print it
+Test Case 1
+Output: 10
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num = 10;
+    printf("%d", num);
+    return 0;
+}
+
+
