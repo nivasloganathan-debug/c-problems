526ad58ad13d46f1d27e7bd7a474d5aafa283a54
Nivas L <nivasloganathan@gmail.com>
2026-02-10 20:27:40 +0530

    Add files via upload

---
+++ change varible value and print updated value.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+ Change variable value and print updated value
+Test Case 1
+Output: 100
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num = 10;   
+    num = 100;   
+    printf("%d", num);
+    return 0;
+}
