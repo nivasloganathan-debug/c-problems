12f46f79c08072465b4d9cb8968563957e44b9a3
Nivas L <nivasloganathan@gmail.com>
2026-02-10 20:14:29 +0530

    Add files via upload

---
+++ Declare a character varaiable and print it.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+
+ Declare a character variable and print it
+Test Case 1
+Output:X
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'X';
+    printf("%c", ch);
+    return 0;
+}
