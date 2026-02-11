c6ede85df4a4e48a03cab6cfb1d56fc0266ffe5e
Nivas L <nivasloganathan@gmail.com>
2026-02-11 10:38:38 +0530

    Add files via upload

---
+++ Print the sum of two integers using .c
@@ -0,0 +1,16 @@
+/******************************************************************************
+   Print the sum of two integers using +.
+Test Case 1
+Input: 5 5
+Output: 10
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    scanf("%d %d", &a, &b); 
+    sum = a + b;             
+    printf("%d", sum);       
+    return 0;
+}
