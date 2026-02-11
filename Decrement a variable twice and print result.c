9187637eb69a6a4bb9c2f5013f9696639c41c8fa
Nivas L <nivasloganathan@gmail.com>
2026-02-11 15:03:18 +0530

    Add files via upload

---
+++ Decrement a variable twice and print result.c
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ Decrement a variable twice and print result.
+Test Case 1
+Input: 6
+Output: 4
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);  
+    num--;   
+    num--;   
+    printf("%d", num);
+    return 0;
+}
+
+
