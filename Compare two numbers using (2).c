d1055e7cddbc51e39f6817ffed119d0412f4f7fb
Nivas L <nivasloganathan@gmail.com>
2026-02-11 15:20:32 +0530

    Add files via upload

---
+++ Compare two numbers using (2).c
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ Topic: Relational Operators (> < == != >= <=)
+Easy Questions
+ Compare two numbers using !=.
+Test Case 1
+Input: 5 3
+Output: 1
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a != b);   
+    return 0;
+}
+
