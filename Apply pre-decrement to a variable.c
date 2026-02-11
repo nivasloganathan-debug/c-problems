cf4f528163108eb7e230330229912906a11e8026
Nivas L <nivasloganathan@gmail.com>
2026-02-11 14:33:07 +0530

    Add files via upload

---
+++ Apply pre-decrement to a variable.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Apply pre-decrement to a variable.
+Test Case 1
+Input: 10
+Output: 9
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);  
+    --num;               
+    printf("%d", num);   
+    return 0;
+}
