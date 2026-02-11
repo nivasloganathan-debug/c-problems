37938b05ab940e0d3ee620982c57e860024ce64d
Nivas L <nivasloganathan@gmail.com>
2026-02-11 20:43:52 +0530

    Add files via upload

---
+++ Assign a value and print it.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Topic: Assignment Operators (= += -= *= /= %=)
+Easy Questions
+1. Assign a value and print it.
+Test Case 1
+Input: 10
+Output: 10
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);   
+    printf("%d", a);    
+    return 0;
+}
