20aecc079c945993df27686cf2a943e3d1540d2f
Nivas L <nivasloganathan@gmail.com>
2026-02-11 11:15:31 +0530

    Add files via upload

---
+++ Print the difference of two integers using -.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+Print the difference of two integers using -.
+Test Case 1
+Input: 8 3
+Output: 5
+*******************************************************************************/
+#include <stdio.h>
+int main() 
+{
+    int a = 8;
+    int b = 3;
+    printf("%d", a - b);
+    return 0;
+}
