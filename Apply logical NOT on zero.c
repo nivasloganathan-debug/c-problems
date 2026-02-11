a01949a89b723b8bcfc28205d7fefd8ecace1418
Nivas L <nivasloganathan@gmail.com>
2026-02-11 15:39:29 +0530

    Add files via upload

---
+++ Apply logical NOT on zero.c
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ Topic: Relational Operators (> < == != >= <=)
+Easy Questions
+  Apply logical NOT on zero.
+Test Case 1
+Input: 0
+Output: 1
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a); 
+    printf("%d", !a);  
+    return 0;
+}
