1cfcecb4958773f4523d6442c03c3758acaa3499
Nivas L <nivasloganathan@gmail.com>
2026-02-11 12:01:35 +0530

    Add files via upload

---
+++ Apply pre-increment to a variable.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+Topic: Increment / Decrement Operators
+(++ --)
+1. Apply pre-increment to a variable.
+Test Case 1
+Input: 5
+Output: 6
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    ++a;
+    printf("%d", a);
+    return 0;
+}
