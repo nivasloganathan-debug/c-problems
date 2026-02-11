79d97ae9034dd1eb4b1744cd589fe598187746dd
Nivas L <nivasloganathan@gmail.com>
2026-02-11 10:22:42 +0530

    Add files via upload

---
+++ size of char.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+topic 3: Data Types in C – Test Case
+ Size of char
+Test Case 1
+Output: 1
+
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    printf("%lu", sizeof(char));
+    return 0;
+}
