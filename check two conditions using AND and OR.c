3a90bf77b99b8d26870d7c6bf70c25abfba2351d
Nivas L <nivasloganathan@gmail.com>
2026-02-11 15:47:38 +0530

    Add files via upload

---
+++ check two conditions using AND and OR.c
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ Topic: Relational Operators (> < == != >= <=)
+Easy Questions
+Check two conditions using AND and OR.
+Test Case 1
+Input: 5 10
+Output: 1
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);   
+    printf("%d", !a);
+    return 0;
+}
