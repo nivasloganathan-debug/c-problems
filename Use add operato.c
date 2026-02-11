2cc3afe917084b5dc4556a13f42f59ff789407f8
Nivas L <nivasloganathan@gmail.com>
2026-02-11 20:48:37 +0530

    Add files via upload

---
+++ Use add operato.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Topic: Assignment Operators (= += -= *= /= %=)
+Easy Questions
+. Use += operator.
+Test Case 1
+Input: 10 5
+Output: 15
+**************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);  
+    a += b;                  
+    printf("%d", a);         
+    return 0;
+}
