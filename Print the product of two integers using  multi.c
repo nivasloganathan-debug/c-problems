f2355e835f9879fb4b0d92321212dbb3da2ea57a
Nivas L <nivasloganathan@gmail.com>
2026-02-11 11:19:12 +0530

    Add files via upload

---
+++ Print the product of two integers using  multi.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+Print the product of two integers using *.
+Test Case 1
+Input: 4 5
+Output: 20
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a * b);
+    return 0;
+}
