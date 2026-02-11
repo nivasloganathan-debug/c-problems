6c3f2a7f091eac3599987d62f19e9d0263d8511a
Nivas L <nivasloganathan@gmail.com>
2026-02-11 14:57:20 +0530

    Add files via upload

---
+++ Increment a variable twice and print result.c
@@ -0,0 +1,17 @@
+/*****************************************************************************
+Increment a variable twice and print result.
+Test Case 1
+Input: 5
+Output: 7
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);  
+    num++;  
+    num++;  
+    printf("%d", num);  
+    return 0;
+}
+
