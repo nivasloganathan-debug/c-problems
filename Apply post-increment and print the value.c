615686a35778ebc95ca0b6d746b94a6de357648f
Nivas L <nivasloganathan@gmail.com>
2026-02-11 12:17:16 +0530

    Add files via upload

---
+++ Apply post-increment and print the value.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+ Apply post-increment and print the value.
+Test Case 1
+Input: 5
+Output: 5
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", a++);
+    return 0;
+}
