99502230cddfc6fff8bd5a28c811a8a1537b5312
Nivas L <nivasloganathan@gmail.com>
2026-02-11 10:14:07 +0530

    Add files via upload

---
+++ size of double.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+topic 3: Data Types in C – Test Case
+Size of double
+Test Case 1
+Output: 8
+
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    printf("%lu", sizeof(double));
+    return 0;
+}
