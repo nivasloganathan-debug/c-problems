0618f528022fb6eea2b41b1d332f86ac7278e49d
Nivas L <nivasloganathan@gmail.com>
2026-02-11 11:38:23 +0530

    Add files via upload

---
+++ Add three integers and print the result.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+Add three integers and print the result.
+Test Case 1
+Input: 2 3 5
+Output: 10
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c = 5;
+    int sum;
+
+    sum = a + b + c;
+    printf("%d", sum);
+    return 0;
+}
