98517f6ca414a122e96aa67dbbf6ee2675385648
Nivas L <nivasloganathan@gmail.com>
2026-02-12 15:18:16 +0530

    Add files via upload

---
+++ type of Triangle.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+Type of Triangle
+Input: 5 5 5
+Output: Equilateral triangle
+Explanation: All sides equal → equilateral
+*******************************************************************************/
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == b && b == c)
+        printf("Equilateral triangle");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles triangle");
+    else
+        printf("Scalene triangle");
+    return 0;
+}
