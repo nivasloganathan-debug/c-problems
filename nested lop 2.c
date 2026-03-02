5f5ca9fa8c2ecd5e041181c500632353091b5253
Nivas L <nivasloganathan@gmail.com>
2026-03-02 15:24:43 +0530

    Add files via upload

---
+++ nested lop 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>en
+int main(){
+    int i,j;
+    int n=5;  
+    for(i=1;i<=n;i++){        
+        for(j = 1; j <= i; j++){
+            printf("%d", i);
+        }
+        printf("\n");                
+    }
+    return 0;
+}
