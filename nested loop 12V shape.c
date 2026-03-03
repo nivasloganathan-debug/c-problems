7a410f47ac10eebbb0cb93c270286eb3c7cbe883
Nivas L <nivasloganathan@gmail.com>
2026-03-03 10:35:04 +0530

    Add files via upload

---
+++ nested loop 12V shape.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int n = 4;
+    int i, j;
+
+    for(i = 1; i <= n; i++) {
+
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        if(i != n) {
+            for(j = 1; j <= 2*(n - i); j++) {
+                printf(" ");
+            }
+        }
+
+        for(j = i; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
