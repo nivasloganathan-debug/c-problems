2f9cdfcfc06548d903af78f514abae232dd41c38
Nivas L <nivasloganathan@gmail.com>
2026-03-05 11:14:15 +0530

    Add files via upload

---
+++ 1d Check Palindrome Sensor Data.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int isPalindrome=1;  
+    for(i=0;i<n/2;i++)
+    {
+        if(arr[i]!=arr[n-i-1])
+        {
+            isPalindrome=0;
+            break;
+        }
+    }
+    if(isPalindrome)
+        printf("Yes");
+    else
+        printf("No");
+    return 0;
+}
