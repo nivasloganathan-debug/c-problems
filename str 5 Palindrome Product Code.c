e08dc905dee9bd3ac77215c981cd4dc8aacc80c4
Nivas L <nivasloganathan@gmail.com>
2026-03-13 15:28:09 +0530

    Add files via upload

---
+++ str 5 Palindrome Product Code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, length = 0, flag = 1;
+    scanf("%s", str);
+    while(str[length] != '\0') {
+        length++;
+    }
+    for(i = 0; i < length / 2; i++) {
+        if(str[i] != str[length - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+    if(flag == 1)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+    return 0;
+}
