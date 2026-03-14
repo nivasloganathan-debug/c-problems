8ea2e2963420fda9655749478361ed87788a839b
Nivas L <nivasloganathan@gmail.com>
2026-03-14 14:12:50 +0530

    Add files via upload

---
+++ str 6 Vowel Counter in Reviews.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+    printf("Enter the review: ");
+    scanf("%s", str);
+    while(str[i] != '\0')
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+        {
+            count++;
+        }
+        i++;
+    }
+    printf("Total vowels: %d", count);
+    return 0;
+}
